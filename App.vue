76a2a1d665ce9d3401a0be74eed757093d160368
Web-szg <50794687+Web-szg@users.noreply.github.com>
2019-05-28 16:53:30 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,62 @@
+<template>
+	
+  <div id="app">
+		<input type="text" v-model="text">
+		<button @click="shanchu">按钮</button>
+		<ul>
+			<li v-for="(a,index) in list" :key="index" @click="asd(index)">
+				{{a}}
+			</li>
+		</ul>
+  </div>
+	
+</template>
+
+<script>
+	export default{
+		data(){
+			return({
+				list:[
+					"奥斯卡就",
+					"51554",
+					"5445"
+				],
+				text:''
+			})
+			
+		},
+		methods:{
+			shanchu(){
+				this.list.push(this.text);
+				this.text=""
+			},
+			asd(index){
+				this.list.splice(index,1)
+			}
+		}
+	}
+</script>
+
+<style>
+		li{
+			list-style-type: none;
+		}
+		#app{
+			margin: 0 auto;
+			width: 220px;
+			height: auto;
+			background: #42B983;
+			border: 1px solid aqua;
+		}
+		input{
+			background: #00FFFF;
+		}
+		button{
+			background: blueviolet;
+			color: white;
+		}
+		 li{
+			border-bottom: 1px solid #00FFFF;
+		}
+		
+</style>
